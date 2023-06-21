f1221e6277d8cd939d6df65d6f533b86ab428ec3
LILANGYU <1577958961@qq.com>
2023-06-21 15:46:00 +0800

    uuu

--- main.ts
+++ main.ts
@@ -608,20 +608,20 @@ namespace Tinybit {
         speed_R = res_y - 0 - res_x;
 
         // 防死区
-        if(speed_L >5 && speed_L <42)
+        if(speed_L >3 && speed_L <speed_min)
         {
             speed_L = speed_min;
         }
-        else if(speed_L <-5 && speed_L > -42)
+        else if(speed_L <-3 && speed_L > -speed_min)
         {
             speed_L = -speed_min;
         }
         
-        if(speed_R >5 && speed_R <42)
+        if(speed_R >3 && speed_R <speed_min)
         {
             speed_R = speed_min;
         }
-        else if(speed_R <-5 && speed_R > -42)
+        else if(speed_R <-3 && speed_R > -speed_min)
         {
             speed_R = -speed_min;
         }
